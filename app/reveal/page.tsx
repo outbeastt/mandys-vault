c9c9d04b6e0c298dec586810a59a289c621702d8
outbeastt <23mabogoane.katlego@gmail.com>
2026-04-15 16:48:51 +0200

    Fixed missing module export in reveal page

---
+++ app/reveal/page.tsx
@@ -0,0 +1,7 @@
+export default function RevealPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1>The Vault Opening Soon...</h1>
+    </div>
+  );
+}
